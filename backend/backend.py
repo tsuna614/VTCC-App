aa8bae33f72d71aa9f378c351d4be6113cbfc48a
dobhuy <21522137@gm.uit.edu.vn>
2023-12-06 11:31:40 +0700

    call python function from nodejs

--- backend/backend.py
+++ backend/backend.py
@@ -2,18 +2,22 @@ import pickle
 from skimage import feature
 import cv2 as cv
 import numpy as np
+import sys
+
 def TinhHOG(pathfilename):
   img = cv.imread(pathfilename,0)
-
   img = cv.resize(img, (64, 128))
-
+  print('3')
   (hog, hog_image) = feature.hog(
       img, orientations=9,
       pixels_per_cell=(8, 8), cells_per_block=(2, 2),
       block_norm='L2-Hys', visualize=True, transform_sqrt=True
       )
+  print('4')
   # return hog / np.linalg.norm(hog)
   return hog
-# load_model = pickle.load(open('E:\computer_vision\Project\svm_hog.sav', 'rb'))
+load_model = pickle.load(open('E:\\MayHocvaCongCu_SE335\\Project\\VTCC-App\\backend\\svm_hog.sav', 'rb'))
 # X_temp=np.array(TinhHOG('E:\computer_vision\Project\BanhPia\Banh_pia_138.png').reshape(1,-1))
-# print(load_model.predict(X_temp))
+# print(X_temp)
+# print(load_model.predict(TinhHOG('E:\computer_vision\Project\BanhPia\Banh_pia_138.png').reshape(1,-1)))
+print(load_model.predict(TinhHOG(sys.argv[1]).reshape(1,-1)))
--- backend/routes/home.route.js
+++ backend/routes/home.route.js
@@ -3,6 +3,22 @@ const path = require("path");
 const router = express.Router();
 const homeController = require("../controllers/home.controller");
 
-router.get("/", homeController.getHomePage);
+// router.get("/", homeController.getHomePage);
+
+router.get("/", (req, res) => {
+  const { spawn } = require("child_process");
+  const pyProg = spawn("python", [
+    "backend.py",
+    "E:\\MayHocvaCongCu_SE335\\Project\\data\\Banh_1.jpg",
+  ]);
+
+  console.log('running');
+
+  pyProg.stdout.on("data", function (data) {
+    console.log(data.toString());
+    // res.write(data);
+    // res.end("end");
+  });
+});
 
 module.exports = router;
