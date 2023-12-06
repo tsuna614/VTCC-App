b2f9a441f5b146758baf4f9b54e7c5307fece84c
Khanh <21520982@ms.uit.edu.vn>
2023-12-06 08:59:51 +0700

    added item controller

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const cors = require("cors");
 require("dotenv").config();
 
 const homeRoute = require("./routes/home.route");
+const itemRoute = require("./routes/item.route");
 
 app.use(homeRoute);
+app.use("/v1/item", itemRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}/`);
